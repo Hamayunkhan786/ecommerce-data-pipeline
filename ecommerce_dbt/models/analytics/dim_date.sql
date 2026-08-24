83bacc6d06def0c3b405d60c99ae4d38dc6e3981
Hamayun Khan <fk385961@gmail.com>
2026-08-24 23:36:09 +0500

    Update dim date formatting

--- ecommerce_dbt/models/analytics/dim_date.sql
+++ ecommerce_dbt/models/analytics/dim_date.sql
@@ -1,10 +1,19 @@
 SELECT DISTINCT
+
     invoice_date::date AS date_key,
+
     EXTRACT(YEAR FROM invoice_date)::integer AS year,
+
     EXTRACT(QUARTER FROM invoice_date)::integer AS quarter,
+
     EXTRACT(MONTH FROM invoice_date)::integer AS month,
-    TO_CHAR(invoice_date, 'Month') AS month_name,
+
+    TO_CHAR(invoice_date, 'FMMonth') AS month_name,
+
     EXTRACT(DAY FROM invoice_date)::integer AS day,
-    TO_CHAR(invoice_date, 'Day') AS day_name
+
+    TO_CHAR(invoice_date, 'FMDay') AS day_name
+
 FROM {{ ref('stg_online_retail') }}
+
 WHERE invoice_date IS NOT NULL
